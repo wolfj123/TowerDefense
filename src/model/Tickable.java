c448970e28cb467581c714488c29dc07f4406111
wolfj123 <wolfj@post.bgu.ac.il>
2017-06-09 13:49:53 +0300

    .

--- src/model/Tickable.java
+++ src/model/Tickable.java
@@ -1,5 +1,20 @@
 package model;
 
-public interface Tickable {
-	void tickHappened();
+public abstract class Tickable {
+	private int _ticks;
+	final int _ticksBeforeAction;
+	
+	public Tickable(int ticksBeforeAction){
+		_ticksBeforeAction = ticksBeforeAction;
+	}
+	
+	public void tickHappened(){
+		_ticks+=1;
+		if(_ticks==_ticksBeforeAction){
+			_ticks = 0;
+			tickAction();
+		}
+	}
+	
+	protected abstract void tickAction();
 }
