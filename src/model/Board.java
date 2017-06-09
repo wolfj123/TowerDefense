adf9d4c48cb4685c373b4143ce5c53ccd8a7ec0f
wolfj123 <wolfj@post.bgu.ac.il>
2017-06-09 12:06:29 +0300

    .

--- src/model/Board.java
+++ src/model/Board.java
@@ -1,21 +1,31 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.Vector;
 
 public class Board implements Tickable{
-	private Cell[][] _board;
+	private Vector<Integer>[][] _directionBoard;
+	private Tower[][] _towers;
+	private Creep[][][] _creeps;
+	private int _xSize;
+	private int _ySize;
+	private Spawner _spawner;
 	
-	public Board(Vector<Integer>[][] matrix){
+	
+	private static int _maxNumOfCreepsInCell = 2;  //To allow a ninja with another creep
+	
+	public Board(Vector<Integer>[][] directionBoard){
+		_directionBoard = directionBoard;
 		
+		_xSize = directionBoard.length;
+		_ySize = directionBoard[0].length;
 		
+		_towers = new Tower[_xSize][_ySize];
+		_creeps = new Creep[_xSize][_ySize][_maxNumOfCreepsInCell];
+	}
+	
+	public Vector<Creep> getCreepsInRange(int x, int y, int range){
 		
 	}
 	
-	public Vector<Creep> getCreepsInRange(int range){
-		
-		//TODO
-		return null;
-	}
-	
-	public 
 }
---
+++ src/model/Spawner.java
@@ -0,0 +1,7 @@
+package model;
+
+public class Spawner implements Tickable{
+	
+	
+	
+}
